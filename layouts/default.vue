bf582c0982d52b254ea4a1e53245ee1c9727b41a
chesh <chesh@obsidian-health.com>
2021-09-26 16:30:17 +0100

    Initial version

---
+++ layouts/default.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-tree',
+          title: 'Welcome',
+          to: '/'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Inclusive Colne Valley'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to Inclusive Colne Valley
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Inclusive Colne Valley aims to provide a variety of resources for
+            various educational uses
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
