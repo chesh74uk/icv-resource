56b2b58b73a2c28eda629edb19b5316ab6ad830f
chesh <chesh@chesh.info>
2021-09-27 16:29:46 +0100

    Added menu and navigation to resource page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,51 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-app-bar
+      dense
       :clipped-left="clipped"
       fixed
-      app
-    />
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
+      app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    </v-app-bar>
+
     <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
+      v-model="drawer"
+      absolute
       temporary
-      fixed
     >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+        >
+          <v-list-item
+          v-for="(item,i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <Nuxt/>
+      </v-container>
+    </v-main>
+
     <v-footer
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; Inclusive Colne Valley {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
@@ -40,7 +54,6 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: false,
       fixed: false,
       items: [
@@ -48,6 +61,11 @@ export default {
           icon: 'mdi-tree',
           title: 'Welcome',
           to: '/'
+        },
+        {
+          icon: 'mdi-wan',
+          title: 'Resources',
+          to: '/resources'
         }
       ],
       miniVariant: false,
