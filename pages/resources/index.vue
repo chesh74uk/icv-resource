dddebce6158a6a2d02791a162578cbc7ed738b83
chesh <chesh@chesh.info>
2021-10-01 17:01:58 +0100

    data tidied up

--- pages/resources/index.vue
+++ pages/resources/index.vue
@@ -10,43 +10,6 @@ import ResourceList from '@/components/Resources/ResourceList'
 export default {
   name: 'Index',
   components: { ResourceList },
-  fetch (context) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line nuxt/no-timing-in-fetch-data
-      setTimeout(() => {
-        resolve({
-          loadedResources: [
-            {
-              id: '1',
-              color: '#1F7087',
-              img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-              link: 'https://www.theguardian.com/uk-news/2021/jul/30/the-links-between-welsh-slate-and-slavery?CMP=Share_iOSApp_Other',
-              title: 'Links between Welsh slate and slavery',
-              summary: 'Steve Cushion on the Pennant family',
-              category: 'Secondary',
-              type: 'Article'
-            },
-            {
-              id: '2',
-              color: '#1F7087',
-              img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-              link: 'https://www.bbc.co.uk/sounds/play/p01w66tg',
-              title: 'Famous Victorians',
-              summary: 'Three stories focusing on the life of Mary Seacole',
-              category: 'Key Stage 2',
-              type: 'Audio'
-            }
-          ]
-        })
-      }, 1000)
-    })
-      .then((data) => {
-        context.store.commit('setResources', data.loadedResources)
-      })
-      .catch((e) => {
-        context.error(e)
-      })
-  },
   computed: {
     loadedResources () {
       return this.$store.getters.loadedResources
--- store/index.js
+++ store/index.js
@@ -11,6 +11,37 @@ const createStore = () => {
       }
     },
     actions: {
+      nuxtServerInit (vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          // eslint-disable-next-line nuxt/no-timing-in-fetch-data
+          setTimeout(() => {
+            vuexContext.commit('setResources',
+              [
+                {
+                  id: '1',
+                  color: '#1F7087',
+                  img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+                  link: 'https://www.theguardian.com/uk-news/2021/jul/30/the-links-between-welsh-slate-and-slavery?CMP=Share_iOSApp_Other',
+                  title: 'Links between Welsh slate and slavery',
+                  summary: 'Steve Cushion on the Pennant family',
+                  category: 'Secondary',
+                  type: 'Article'
+                },
+                {
+                  id: '2',
+                  color: '#1F7087',
+                  img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+                  link: 'https://www.bbc.co.uk/sounds/play/p01w66tg',
+                  title: 'Famous Victorians',
+                  summary: 'Three stories focusing on the life of Mary Seacole',
+                  category: 'Key Stage 2',
+                  type: 'Audio'
+                }
+              ])
+            resolve()
+          }, 1000)
+        })
+      },
       setResources (vuexContext, resources) {
         vuexContext.commit('setResources', resources)
       }
