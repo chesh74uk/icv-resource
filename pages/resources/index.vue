5315b0dfb809ea89ff1a3bc087d5455e9b5d8c3c
chesh <chesh@chesh.info>
2021-10-01 16:19:09 +0100

    hands!

--- pages/resources/index.vue
+++ pages/resources/index.vue
@@ -10,35 +10,43 @@ import ResourceList from '@/components/Resources/ResourceList'
 export default {
   name: 'Index',
   components: { ResourceList },
-  asyncData (context, callback) {
-    console.log(context)
-    // eslint-disable-next-line nuxt/no-timing-in-fetch-data
-    setTimeout(() => {
-      callback(null, {
-        loadedResources: [
-          {
-            id: '1',
-            color: '#1F7087',
-            img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-            link: 'https://www.theguardian.com/uk-news/2021/jul/30/the-links-between-welsh-slate-and-slavery?CMP=Share_iOSApp_Other',
-            title: 'Links between Welsh slate and slavery',
-            summary: 'Steve Cushion on the Pennant family',
-            category: 'Secondary',
-            type: 'Article'
-          },
-          {
-            id: '2',
-            color: '#1F7087',
-            img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-            link: 'https://www.bbc.co.uk/sounds/play/p01w66tg',
-            title: 'Famous Victorians',
-            summary: 'Three stories focusing on the life of Mary Seacole',
-            category: 'Key Stage 2',
-            type: 'Audio'
-          }
-        ]
+  asyncData (context) {
+    return new Promise((resolve, reject) => {
+      // eslint-disable-next-line nuxt/no-timing-in-fetch-data
+      setTimeout(() => {
+        resolve({
+          loadedResources: [
+            {
+              id: '1',
+              color: '#1F7087',
+              img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+              link: 'https://www.theguardian.com/uk-news/2021/jul/30/the-links-between-welsh-slate-and-slavery?CMP=Share_iOSApp_Other',
+              title: 'Links between Welsh slate and slavery',
+              summary: 'Steve Cushion on the Pennant family',
+              category: 'Secondary',
+              type: 'Article'
+            },
+            {
+              id: '2',
+              color: '#1F7087',
+              img: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+              link: 'https://www.bbc.co.uk/sounds/play/p01w66tg',
+              title: 'Famous Victorians',
+              summary: 'Three stories focusing on the life of Mary Seacole',
+              category: 'Key Stage 2',
+              type: 'Audio'
+            }
+          ]
+        })
+      }, 1000)
+    })
+      .then((data) => {
+        return data
       })
-    }, 1000)
+      .catch((e) => {
+        context.error(new Error('error'))
+      })
+    // eslint-disable-next-line nuxt/no-timing-in-fetch-data
   },
   // data () {
   //   return {
